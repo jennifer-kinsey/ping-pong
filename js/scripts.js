776020784d2da50fcec1cc3d04230bf288ea9578
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-03-31 09:09:05 -0700

    create a function that will make an array from 1 to input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
  // *********************BUSINESS LOGIC*************************
+    //make an an array out of the numbers
 
+    var resultArray = [];
+
+    var makeArray = function(input){
+      for(var i = 0; i <= input; i++){
+        resultArray.push(i);
+      }
+      return resultArray;
+    }
+
+    var replaceThird
 
 
 
   // *********************FRONT END LOGIC*************************
   $(function() {
 
+    //submit form function
     $("form").submit(function(e) {
       e.preventDefault();
       var userInput = parseInt($("#userInput").val());
+
+      makeArray(userInput);
+      console.log(resultArray);
+
       $("form").hide();
       $("#result").show();
     });
+
 
     //reload button function
     $("#reload").click(function(){
